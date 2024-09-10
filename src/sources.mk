088c65b4276c5475e35376876fb250615ead9892
Abdelrahman <abdoabomoosa739@gmail.com>
2024-09-10 16:30:33 +0300

    Adding all source files pathes and all necessary includes to both platforms using conditional check

--- src/sources.mk
+++ src/sources.mk
@@ -1,17 +1,31 @@
 #******************************************************************************
-# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+# Copyright (C) 2024 by Abdelrahman Abomosa
 #
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are 
 # permitted to modify this and use it to learn about the field of embedded
-# software. Alex Fosdick and the University of Colorado are not liable for any
+# software. Abdelrahman Abomosa and the University of Colorado are not liable for any
 # misuse of this material. 
 #
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+ifeq ($(PLATFORM), MSP432)
+        SOURCES = *.c 
 
-# Add your include paths to this variable
-INCLUDES = 
+        # specify the includes directory
+        INCLUDES_DIR = $(home)/week2/includes
+
+        # Add your include paths to this variable
+        INCLUDES = $(shell find $(INCLUDES-DIR) -type f)
+else
+        SOURCES = main.c \
+                  memory.c
+
+        INCLUDES = ../includes/common/memorey.h \
+                   ../includes/common/platform.h
+
+endif
+
+
 
